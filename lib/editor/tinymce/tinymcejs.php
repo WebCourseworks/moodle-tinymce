b79f1dc67e735fd0f0c9063537ecc10483e071fa
John Ehringer <jehringer@webcourseworks.com>
2011-05-19 11:25:47 -0500

    Just a start on the tinymce integration code.  Commiting my work in progress.

---
+++ lib/editor/tinymce/tinymcejs.php
@@ -0,0 +1,15 @@
+<?
+	// In this file we'll need to configure the tinymce editor correctly and hijack the HTMLArea object to handle initialization of TinyMCE.
+?>
+
+HTMLArea = function(textareaId, config) {
+	
+}
+
+HTMLArea.prototype.replaceAll = function(config) {
+	
+}
+
+HTMLArea.prototype.generate = function() {
+	
+}
---
+++ lib/tinymcelib.php
@@ -0,0 +1,16 @@
+<?
+
+global $CFG;
+
+// Disable the HTML area code from being included in print_textarea.
+unset($CFG->editorsrc);
+
+/**
+ * Output the necessary TinyMCE JavaScript for javascript.php.
+ */
+function tinymce_output_page_javascript() {
+	global $CFG;
+
+	echo "<script type=\"text/javascript\" src=\"{$CFG->httpswwwroot}/lib/editor/tinymce/jscripts/tiny_mce/tiny_mce.js\"></script>";
+	echo "<script type=\"text/javascript\" src=\"{$CFG->httpswwwroot}/lib/editor/tinymce/tinymcejs.php\"></script>";
+}
