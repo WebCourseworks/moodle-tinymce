18010e93c20d7492861b6d31d0ead3a504c392ee
John Ehringer <jehringer@webcourseworks.com>
2011-06-05 15:46:00 -0500

    Copying the JavaScript headers from htmlarea.php for tinymcejs.php.

--- lib/editor/tinymce/tinymcejs.php
+++ lib/editor/tinymce/tinymcejs.php
@@ -4,6 +4,17 @@ require_once('../../../config.php');
 
 global $CFG; 
 
+/* Uncomment after dev, before qa.
+$lastmodified = filemtime("htmlarea.php");
+$lifetime = 1800;
+
+header("Content-type: application/x-javascript; charset: utf-8");  // Correct MIME type
+header("Last-Modified: " . gmdate("D, d M Y H:i:s", $lastmodified) . " GMT");
+header("Expires: " . gmdate("D, d M Y H:i:s", time() + $lifetime) . " GMT");
+header("Cache-control: max_age = $lifetime");
+header("Pragma: ");
+*/
+
 define('MENU_SEPARATOR', '|');
 
 $courseid = empty($COURSE->id) ? 1 : $COURSE->id;
@@ -33,7 +44,29 @@ $tinymceplugins = array('safari',
                         'template', 
                         'inlinepopups');
 
-
+// setup coversion table for swapping HTMLarea controls to TinyMCE controls
+$tinymcebuttonmap = array('fontname'             => 'fontselect', 
+                          'fontsize'             => 'fontsizeselect', 
+                          'formatblock'          => 'blockquote', 
+                          'subscript'            => 'sub', 
+                          'superscript'          => 'sup', 
+                          'clean'                => 'cleanup', 
+                          'lefttoright'          => 'ltr', 
+                          'righttoleft'          => 'rtl', 
+                          'insertorderedlist'    => 'numlist', 
+                          'insertunorderedlist'  => 'bullist', 
+//                          'hilitecolor'         => '', 
+                          'inserthorizontalrule' => 'hr', 
+                          'createanchor'         => 'anchor', 
+                          'createlink'           => 'link', 
+                          'insertimage'          => 'image', 
+                          'inserttable'          => 'tablecontrols', 
+                          'insertsmile'          => 'moodlesmileys', 
+                          'insertchar'           => 'charmap', 
+                          'spellcheck'           => 'spellchecker', 
+                          'htmlmode'             => 'code', 
+                          'popupeditor'          => 'fullscreen', 
+                          'search_replace'       => 'search,replace');
 
 //setup array of buttons to hide and convert them to TinyMCE if needed.
 $hidebuttons =  array();
@@ -75,172 +108,146 @@ if(!empty($CFG->editorbackgroundcolor)) {
     $editorbackgroundcolor = $CFG->editorbackgroundcolor;
 }
 
-function tinymcemaphtmlareabutton($htmlareabutton) {
-    // setup coversion table for swapping HTMLarea controls to TinyMCE controls
-    $tinymcebuttonmap = array('fontname'             => 'fontselect', 
-                              'fontsize'             => 'fontsizeselect', 
-                              'formatblock'          => 'blockquote', 
-                              'subscript'            => 'sub', 
-                              'superscript'          => 'sup', 
-                              'clean'                => 'cleanup', 
-                              'lefttoright'          => 'ltr', 
-                              'righttoleft'          => 'rtl', 
-                              'insertorderedlist'    => 'numlist', 
-                              'insertunorderedlist'  => 'bullist', 
-//                             'hilitecolor'         => '', 
-                              'inserthorizontalrule' => 'hr', 
-                              'createanchor'         => 'anchor', 
-                              'createlink'           => 'link', 
-                              'insertimage'          => 'image', 
-                              'inserttable'          => 'tablecontrols', 
-                              'insertsmile'          => 'moodlesmileys', 
-                              'insertchar'           => 'charmap', 
-                              'spellcheck'           => 'spellchecker', 
-                              'htmlmode'             => 'code', 
-                              'popupeditor'          => 'fullscreen', 
-                              'search_replace'       => 'search,replace');
-}
-
 ?>
 
 HTMLArea = function() {
-    var id;
-    var cfg;
+	var id;
+	var cfg;
 
-    if (arguments.length == 0) {
-        return;
-    }
+	if (arguments.length == 0) {
+		return;
+	}
 
     var id = arguments[0];
 
     // Default config for this area.
     var config = {
-        pageStyle : 'body { background-color: #ffffff; }',
-        killWordOnPaste : true,
-        fontname : {
-            // This is the default font list from HtmlArea.Config.
-            'Arial'           : 'arial,helvetica,sans-serif',
-            'Courier New'     : 'courier new,courier,monospace',
-            'Georgia'         : 'georgia,times new roman,times,serif',
-            'Tahoma'          : 'tahoma,arial,helvetica,sans-serif',
-            'Times New Roman' : 'times new roman,times,serif',
-            'Verdana'         : 'verdana,arial,helvetica,sans-serif',
-            'Impact'          : 'impact',
-            'WingDings'       : 'wingdings'
-        },
-        fontsize : {
-            // This is the default font size list from HtmlArea.Config.
-            '1 (8 pt)'  : '8pt', 
-            '2 (10 pt)' : '10pt', 
-            '3 (12 pt)' : '12pt', 
-            '4 (14 pt)' : '14pt', 
-            '5 (18 pt)' : '18pt', 
-            '6 (24 pt)' : '24pt', 
-            '7 (36 pt)' : '36pt'
-        }
+    	pageStyle : 'body { background-color: #ffffff; }',
+    	killWordOnPaste : true,
+    	fontname : {
+    		// This is the default font list from HtmlArea.Config.
+    		'Arial'           : 'arial,helvetica,sans-serif',
+	        'Courier New'     : 'courier new,courier,monospace',
+	        'Georgia'         : 'georgia,times new roman,times,serif',
+	        'Tahoma'          : 'tahoma,arial,helvetica,sans-serif',
+	        'Times New Roman' : 'times new roman,times,serif',
+	        'Verdana'         : 'verdana,arial,helvetica,sans-serif',
+	        'Impact'          : 'impact',
+	        'WingDings'       : 'wingdings'
+    	},
+    	fontsize : {
+    		// This is the default font size list from HtmlArea.Config.
+	        '1 (8 pt)'  : '8pt', 
+	        '2 (10 pt)' : '10pt', 
+	        '3 (12 pt)' : '12pt', 
+	        '4 (14 pt)' : '14pt', 
+	        '5 (18 pt)' : '18pt', 
+	        '6 (24 pt)' : '24pt', 
+	        '7 (36 pt)' : '36pt'
+	    }
     };
 
     // Merge in supplied arguments.
     if (arguments.length > 1) {
-        for (arg in arguments[1]) {
-            config[arg] = arguments[1][arg];
-        }
+    	for (arg in arguments[1]) {
+    		config[arg] = arguments[1][arg];
+    	}
     }
-
+    
     var compileFontList = function(list) {
-        // http://tinymce.moxiecode.com/wiki.php/Configuration:theme_advanced_fonts
-        // http://tinymce.moxiecode.com/wiki.php/Configuration:theme_advanced_font_sizes
-        var results = [];
+    	// http://tinymce.moxiecode.com/wiki.php/Configuration:theme_advanced_fonts
+    	// http://tinymce.moxiecode.com/wiki.php/Configuration:theme_advanced_font_sizes
+    	var results = [];
 
-        for(item in list) {
-            results.push(item + '=' + list[item]);
-        }
+    	for(item in list) {
+    		results.push(item + '=' + list[item]);
+    	}
 
-        return results.join(';');
-    };
+    	return results.join(';');
+    }
 
     var generate = function() {
-        tinyMCE.init({
-            // Editor selection
-            mode : "exact", 
-            elements : id, 
-            // Plugins
-            plugins : "<?php echo implode(',', $tinymceplugins); ?>", 
-            // General config
-            convert_urls : false, 
-            add_form_submit_trigger : true, 
-            add_unload_trigger : true, 
-            // Browser
-            file_browser_callback : 'moodleFileBrowser',
-            // Theme
-            theme : "advanced", 
-            theme_advanced_buttons1 : "<?php echo $theme_advanced_buttons['theme_advanced_buttons1'] ?>", 
-            theme_advanced_buttons2 : "<?php echo $theme_advanced_buttons['theme_advanced_buttons2'] ?>", 
-            theme_advanced_buttons3 : "<?php echo $theme_advanced_buttons['theme_advanced_buttons3'] ?>", 
-            theme_advanced_toolbar_location : "top", 
-            theme_advanced_toolbar_align : "left", 
-            theme_advanced_statusbar_location : "bottom", 
-            theme_advanced_resizing : true,
-            theme_advanced_fonts : compileFontList(config.fontname),
-            theme_advanced_font_sizes : compileFontList(config.fontsize)
-        });
-    };
+		tinyMCE.init({
+			// Editor selection
+			mode : "exact", 
+			elements : id, 
+			// Plugins
+			plugins : "<?php echo implode(',', $tinymceplugins); ?>", 
+			// General config
+	        convert_urls : false, 
+	        add_form_submit_trigger : true, 
+			add_unload_trigger : true, 
+	        // Browser
+			file_browser_callback : 'moodleFileBrowser',
+			// Theme
+			theme : "advanced", 
+			theme_advanced_buttons1 : "<?php echo $theme_advanced_buttons['theme_advanced_buttons1'] ?>", 
+			theme_advanced_buttons2 : "<?php echo $theme_advanced_buttons['theme_advanced_buttons2'] ?>", 
+			theme_advanced_buttons3 : "<?php echo $theme_advanced_buttons['theme_advanced_buttons3'] ?>", 
+	        theme_advanced_toolbar_location : "top", 
+			theme_advanced_toolbar_align : "left", 
+			theme_advanced_statusbar_location : "bottom", 
+			theme_advanced_resizing : true,
+			theme_advanced_fonts : compileFontList(config.fontname),
+			theme_advanced_font_sizes : compileFontList(config.fontsize)
+		});
+	};
 
-    var replaceAll = function() {
-        tinyMCE.init({
-            // Editor selection
-            mode : "textareas", 
-            // Plugins
-            plugins : "<?php echo implode(',', $tinymceplugins); ?>", 
-            // General config
-            convert_urls : false, 
-            // Browser
-            file_browser_callback : 'moodleFileBrowser', 
-            // Theme options
-            theme : "advanced", 
-            theme_advanced_buttons1 : "<?php echo $theme_advanced_buttons['theme_advanced_buttons1'] ?>", 
-            theme_advanced_buttons2 : "<?php echo $theme_advanced_buttons['theme_advanced_buttons2'] ?>", 
-            theme_advanced_buttons3 : "<?php echo $theme_advanced_buttons['theme_advanced_buttons3'] ?>", 
-            theme_advanced_toolbar_location : "top", 
-            theme_advanced_toolbar_align : "left", 
-            theme_advanced_statusbar_location : "bottom", 
-            theme_advanced_resizing : true, 
-            theme_advanced_fonts : compileFontList(config.fontname),
-            theme_advanced_font_sizes : compileFontList(config.fontsize)
-        });
-    };
+	var replaceAll = function() {
+		tinyMCE.init({
+			// Editor selection
+			mode : "textareas", 
+			// Plugins
+			plugins : "<?php echo implode(',', $tinymceplugins); ?>", 
+			// General config
+	        convert_urls : false, 
+	        // Browser
+			file_browser_callback : 'moodleFileBrowser', 
+			// Theme options
+			theme : "advanced", 
+			theme_advanced_buttons1 : "<?php echo $theme_advanced_buttons['theme_advanced_buttons1'] ?>", 
+			theme_advanced_buttons2 : "<?php echo $theme_advanced_buttons['theme_advanced_buttons2'] ?>", 
+			theme_advanced_buttons3 : "<?php echo $theme_advanced_buttons['theme_advanced_buttons3'] ?>", 
+	        theme_advanced_toolbar_location : "top", 
+			theme_advanced_toolbar_align : "left", 
+			theme_advanced_statusbar_location : "bottom", 
+			theme_advanced_resizing : true, 
+			theme_advanced_fonts : compileFontList(config.fontname),
+			theme_advanced_font_sizes : compileFontList(config.fontsize)
+		});
+	};
 
-    return {
-        generate : function() { return generate(); },
-        replaceAll : function() { return replaceAll(); },
-        config : config
-    }
+	return {
+		generate : function() { return generate(); },
+		replaceAll : function() { return replaceAll(); },
+		config : config
+	}
 };
 
 function moodleFileBrowser (field_name, url, type, win) {
-    var cmsURL = '', 
-        width = 0, 
-        height = 0,
-        courseid = <?php  echo $courseid; ?>;
+	var cmsURL = '', 
+	    width = 0, 
+	    height = 0,
+	    courseid = <?php  echo $courseid; ?>;
 
     switch(type) {
-        default:
-        case 'file':
-            cmsURL = '<?php echo $CFG->httpswwwroot; ?>/lib/editor/link.php';
-            width = 480;
-            height = 400;
-            break;
-        case 'image':
-            cmsURL = '<?php echo $CFG->httpswwwroot; ?>/lib/editor/insert_image.php';
-            width = 736;
-            height = 430;
-            break;
-        case 'media':
-            cmsURL = '<?php echo $CFG->httpswwwroot; ?>/lib/editor/insert_image.php';
-            width = 736;
-            height = 430;
-            break;
-    }
+    	default:
+    	case 'file':
+			cmsURL = '<?php echo $CFG->httpswwwroot; ?>/lib/editor/link.php';
+			width = 480;
+			height = 400;
+			break;
+		case 'image':
+			cmsURL = '<?php echo $CFG->httpswwwroot; ?>/lib/editor/insert_image.php';
+			width = 736;
+			height = 430;
+			break;
+		case 'media':
+			cmsURL = '<?php echo $CFG->httpswwwroot; ?>/lib/editor/insert_image.php';
+			width = 736;
+			height = 430;
+			break;
+	}
 
     tinyMCE.activeEditor.windowManager.open({
         file : cmsURL + "?id=" + courseid,
